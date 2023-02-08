11549c5c1cbe4b132662de9da1e4de909a59226a
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-02-08 17:26:04 +0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Homework1Component } from './components/homework1/homework1.component';
+import { Homework2Component } from './components/homework2/homework2.component';
+import { Homework3Component } from './components/homework3/homework3.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Homework1Component,
+    Homework2Component,
+    Homework3Component,
+    NotFoundComponent,
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
