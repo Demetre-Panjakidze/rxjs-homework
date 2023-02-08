4cb75e999cbbe9f7e821b8a2799176d2bc4e2776
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-02-08 18:27:18 +0400

    intial commit

--- src/app/components/homework1/homework1.component.ts
+++ src/app/components/homework1/homework1.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+  id: number;
+}
 
 @Component({
   selector: 'app-homework1',
   templateUrl: './homework1.component.html',
   styleUrls: ['./homework1.component.scss'],
 })
-export class Homework1Component {}
+export class Homework1Component {
+  itemsList: Person[] = [
+    {
+      id: 1,
+      firstName: 'Demetre',
+      lastName: 'Panjakidze',
+      age: 19,
+    },
+    {
+      id: 2,
+      firstName: 'Kote',
+      lastName: 'Kirkitadze',
+      age: 23,
+    },
+    {
+      id: 3,
+      firstName: 'Giorgi',
+      lastName: 'Bazerashvili',
+      age: 27,
+    },
+  ];
+
+  search$ = new Observable();
+}
