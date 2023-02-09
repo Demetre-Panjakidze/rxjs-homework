69554bb8e8de15d28557908791f172a42cecc46f
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-02-09 15:51:54 +0400

    intial commit

--- src/app/components/homework1/homework1.component.ts
+++ src/app/components/homework1/homework1.component.ts
@@ -3,6 +3,15 @@ import { FormControl } from '@angular/forms';
 import { Observable, of, from, switchMap, map, debounceTime } from 'rxjs';
 import { About } from 'src/app/interfaces/IHomework';
 
+class AboutPerson {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public age: number,
+    public id: number
+  ) {}
+}
+
 @Component({
   selector: 'app-homework1',
   templateUrl: './homework1.component.html',
@@ -12,24 +21,36 @@ export class Homework1Component {
   inputContent = new FormControl();
 
   itemsList: About[] = [
-    {
-      id: 1,
-      firstName: 'Demetre',
-      lastName: 'Panjakidze',
-      age: 19,
-    },
-    {
-      id: 2,
-      firstName: 'Kote',
-      lastName: 'Kirkitadze',
-      age: 23,
-    },
-    {
-      id: 3,
-      firstName: 'Giorgi',
-      lastName: 'Bazerashvili',
-      age: 27,
-    },
+    new AboutPerson('John', 'Doe', 30, 1),
+    new AboutPerson('Jane', 'Doe', 28, 2),
+    new AboutPerson('James', 'Smith', 32, 3),
+    new AboutPerson('Emily', 'Johnson', 29, 4),
+    new AboutPerson('William', 'Brown', 33, 5),
+    new AboutPerson('Ava', 'Davis', 27, 6),
+    new AboutPerson('Olivia', 'Miller', 31, 7),
+    new AboutPerson('Sophia', 'Wilson', 28, 8),
+    new AboutPerson('Isabella', 'Moore', 30, 9),
+    new AboutPerson('Mia', 'Taylor', 32, 10),
+    new AboutPerson('Charlotte', 'Anderson', 29, 11),
+    new AboutPerson('Amelia', 'Thomas', 27, 12),
+    new AboutPerson('Harper', 'Jackson', 31, 13),
+    new AboutPerson('Evelyn', 'White', 28, 14),
+    new AboutPerson('Abigail', 'Harris', 30, 15),
+    new AboutPerson('Emily', 'Martin', 32, 16),
+    new AboutPerson('Elizabeth', 'Thompson', 29, 17),
+    new AboutPerson('Avery', 'Garcia', 27, 18),
+    new AboutPerson('Ella', 'Martinez', 31, 19),
+    new AboutPerson('Madison', 'Robinson', 28, 20),
+    new AboutPerson('Scarlett', 'Clark', 30, 21),
+    new AboutPerson('Victoria', 'Rodriguez', 32, 22),
+    new AboutPerson('Aria', 'Lewis', 29, 23),
+    new AboutPerson('Adalynn', 'Walker', 27, 24),
+    new AboutPerson('Aubrey', 'Hall', 31, 25),
+    new AboutPerson('Aurora', 'Allen', 28, 26),
+    new AboutPerson('Eleanor', 'King', 30, 27),
+    new AboutPerson('Natalie', 'Wright', 32, 28),
+    new AboutPerson('Riley', 'Scott', 29, 29),
+    new AboutPerson('Audrey', 'Green', 27, 30),
   ];
 
   search$ = of(this.itemsList);
@@ -50,11 +71,7 @@ export class Homework1Component {
                     .includes(value.toLowerCase())
               )
             ),
-            map((e) =>
-              e.map((x) => {
-                return `${x.firstName} ${x.lastName}`;
-              })
-            )
+            map((e) => e.map((x) => `${x.firstName} ${x.lastName}`))
           );
         })
       )
--- src/app/components/homework2/homework2.component.ts
+++ src/app/components/homework2/homework2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Job, Person } from 'src/app/interfaces/IHomework';
 
 @Component({
@@ -6,4 +7,48 @@ import { Job, Person } from 'src/app/interfaces/IHomework';
   templateUrl: './homework2.component.html',
   styleUrls: ['./homework2.component.scss'],
 })
-export class Homework2Component {}
+export class Homework2Component {
+  getPeople(job: string[]) {
+    return new Observable((x) => {});
+  }
+
+  jobs: Job[] = [
+    { id: 1, name: 'Software Engineer' },
+    { id: 2, name: 'Product Manager' },
+    { id: 3, name: 'Data Scientist' },
+    { id: 4, name: 'Designer' },
+    { id: 5, name: 'DevOps Engineer' },
+    { id: 6, name: 'QA Engineer' },
+    { id: 7, name: 'Project Manager' },
+    { id: 8, name: 'Business Analyst' },
+    { id: 9, name: 'IT Support' },
+    { id: 10, name: 'Full Stack Developer' },
+  ];
+
+  people: Person[] = [
+    { id: 1, jobId: 1, name: 'John', lastname: 'Doe' },
+    { id: 2, jobId: 5, name: 'Jane', lastname: 'Doe' },
+    { id: 3, jobId: 9, name: 'Bob', lastname: 'Smith' },
+    { id: 4, jobId: 6, name: 'Alice', lastname: 'Johnson' },
+    { id: 5, jobId: 3, name: 'Charlie', lastname: 'Brown' },
+    { id: 6, jobId: 6, name: 'Eve', lastname: 'Miller' },
+    { id: 7, jobId: 7, name: 'Mallory', lastname: 'Davis' },
+    { id: 8, jobId: 10, name: 'Peggy', lastname: 'Wilson' },
+    { id: 9, jobId: 2, name: 'Victor', lastname: 'Taylor' },
+    { id: 10, jobId: 5, name: 'Oscar', lastname: 'Anderson' },
+    { id: 11, jobId: 7, name: 'Alex', lastname: 'Thomas' },
+    { id: 12, jobId: 4, name: 'Ava', lastname: 'Moore' },
+    { id: 13, jobId: 8, name: 'Eli', lastname: 'Jackson' },
+    { id: 14, jobId: 10, name: 'Lila', lastname: 'Martin' },
+    { id: 15, jobId: 1, name: 'Nina', lastname: 'Lee' },
+    { id: 16, jobId: 2, name: 'Ella', lastname: 'Harris' },
+    { id: 17, jobId: 8, name: 'Sofia', lastname: 'Young' },
+    { id: 18, jobId: 9, name: 'Aria', lastname: 'Allen' },
+    { id: 19, jobId: 6, name: 'Evelyn', lastname: 'King' },
+    { id: 20, jobId: 7, name: 'Isabelle', lastname: 'Wright' },
+  ];
+
+  // getPeople(['Developer', 'Doctor']).subscribe(x => console.log(x))
+
+  // x => ['ani mchedlidze is a developer', 'levan begashvili is a developer', 'giorgi bazera is a doctor']
+}
